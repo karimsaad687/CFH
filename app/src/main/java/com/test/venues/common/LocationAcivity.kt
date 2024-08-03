06e9ddf906451b0b296bb198cba640a5bcbfef38
karimsaad687 <karimsaad687@gmail.com>
2024-08-03 19:08:22 +0300

    check fo location service

--- app/src/main/java/com/test/venues/common/LocationAcivity.kt
+++ app/src/main/java/com/test/venues/common/LocationAcivity.kt
@@ -1,11 +1,16 @@
 package com.test.venues.common
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -14,6 +19,7 @@ import androidx.core.content.ContextCompat
 
 abstract class LocationAcivity : AppCompatActivity() {
 
+    var fetchLocationOnce=true
     private val requestLocationPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { results ->
@@ -72,15 +78,60 @@ abstract class LocationAcivity : AppCompatActivity() {
             LocationManager.GPS_PROVIDER, 100,
             100f, mLocationListener
         )
+        locationManager.requestLocationUpdates(
+            LocationManager.NETWORK_PROVIDER, 100,
+            100f, mLocationListener
+        )
+        islocationEnabled()
     }
 
     private val mLocationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
-            onLocationFetched(location.latitude, location.longitude)
+            if(fetchLocationOnce) {
+                onLocationFetched(location.latitude, location.longitude)
+                fetchLocationOnce=false
+            }
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
         }
     }
 
     abstract fun onLocationFetched(lat: Double, lng: Double)
 
     abstract fun permissionStatusChanged(flag: Boolean)
+
+
+    fun islocationEnabled() {
+        val lm = getSystemService(LOCATION_SERVICE) as LocationManager
+        var gps_enabled = false
+        var network_enabled = false
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (!gps_enabled && !network_enabled) {
+            AlertDialog.Builder(this@LocationAcivity)
+                .setMessage("GPS Enable")
+                .setPositiveButton("Settings",
+                    DialogInterface.OnClickListener { paramDialogInterface, paramInt ->
+                        startActivity(
+                            Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                        )
+                    })
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
+    }
 }
